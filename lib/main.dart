0eed4eba531b7d1e2d10d59b538fa16a5c264dab
Maulik Shah <maulik.shah715@gmail.com>
2021-07-21 13:50:01 +0530

    Added all the UI buttons in the correct order and location

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,123 @@ class CalcApp extends StatelessWidget {
       title: 'Calculator',
       home: Scaffold(
         backgroundColor: Color(0xFF283637),
-        body: Center(
-          child: CalcButton(
-            text: "1",
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CalcButton(
+                  text: "AC",
+                  fillColor: 0xFF6C807F,
+                  textSize: 22.0,
+                ),
+                CalcButton(
+                  text: "C",
+                  fillColor: 0xFF6C807F,
+                ),
+                CalcButton(
+                  text: "%",
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                ),
+                CalcButton(
+                  text: "/",
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CalcButton(
+                  text: "7",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "8",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "9",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "*",
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CalcButton(
+                  text: "4",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "5",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "6",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "-",
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CalcButton(
+                  text: "1",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "2",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "3",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "+",
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CalcButton(
+                  text: ".",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "0",
+                  fillColor: 0xFF283637,
+                ),
+                CalcButton(
+                  text: "00",
+                  fillColor: 0xFF283637,
+                  textSize: 26.0,
+                ),
+                CalcButton(
+                  text: "=",
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/widgets/CalcButton.dart
+++ lib/widgets/CalcButton.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CalcButton extends StatelessWidget {
   final String text;
+  final int textColor;
+  final int fillColor;
+  final double textSize;
 
-  CalcButton({ required this.text });
+  const CalcButton({
+    Key? key,
+    required this.text,
+    this.textColor = 0xFFFFFFFF,
+    required this.fillColor,
+    this.textSize = 28.0,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 65.0,
-      height: 65.0,
-      child: FlatButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(50.0)
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: SizedBox(
+        width: 65.0,
+        height: 65.0,
+        child: FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.0)
+          ),
+          child: Text(
+            text,
+            style: GoogleFonts.rubik(
+              textStyle: TextStyle(
+                fontSize: textSize,
+              ),
+            ),
+          ),
+          onPressed: () {},
+          color: fillColor != null ? Color(fillColor) : null,
+          textColor: Color(textColor),
         ),
-        child: Text(text),
-        onPressed: () {},
-        color: Colors.blue,
       ),
     );
   }
